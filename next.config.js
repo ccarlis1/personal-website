7c79cf0482490323f24d093335c657a12eb95a9a
sle <135905727+ccarlis1@users.noreply.github.com>
2025-04-22 18:00:16 -0400

    setup github pages config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // If your repository name is different from your username, 
+  // you'll need to set the basePath to match your repository name
+  // basePath: '/your-repo-name',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
