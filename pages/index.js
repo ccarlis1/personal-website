543d4183a87141ee8410d0a2807bcf5281d585e2
sle <135905727+ccarlis1@users.noreply.github.com>
2025-04-22 18:10:35 -0400

    fix build error

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>My Developer Portfolio</h1>
+      <p>Welcome to my personal website! This site is currently under construction.</p>
+    </div>
+  )
+} 
