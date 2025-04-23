842ecc5bbdc05903048cdb870340c3d855b26247
sle <135905727+ccarlis1@users.noreply.github.com>
2025-04-23 15:40:16 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   // If your repository name is different from your username, 
   // you'll need to set the basePath to match your repository name
-  // basePath: '/your-repo-name',
+  // basePath: '/personal-website',
   images: {
     unoptimized: true,
   },
