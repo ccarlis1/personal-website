028144923d599a57a5f4c92ce019941403805239
sle <135905727+ccarlis1@users.noreply.github.com>
2025-04-23 15:05:32 -0400

    reset

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>My Developer Portfolio</h1>
-      <p>Welcome to my personal website! This site is currently under construction.</p>
-    </div>
-  )
-} 
